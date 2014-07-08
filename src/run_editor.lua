0047d17c4ace5a15951af4b0aead0767aecf555a
Alban Linard <alban.linard@lsv.ens-cachan.fr>
2014-07-08 23:25:46 +0200

    Fix the nohup problem.

--- src/run_editor.lua
+++ src/run_editor.lua
@@ -1,5 +1,9 @@
 #! /usr/bin/env lua
 
+local logging   = require "logging"
+logging.console = require "logging.console"
+local logger = logging.console "%level %message\n"
+
 local lfs       = require "lfs"
 local json      = require "dkjson"
 local cli       = require "cliargs"
@@ -27,6 +31,10 @@ cli:add_argument (
   "editor-token",
   "administration token for the editor"
 )
+cli:add_flag(
+  "-v, --verbose",
+  "enable verbose mode"
+)
 
 local editor_defaults = require "editor"
 local args = cli:parse_args ()
@@ -43,6 +51,21 @@ local dispatcher_token  = args ["dispatcher-token"]
 local editor_token      = args ["editor-token"]
 local editor_port       = editor_defaults.port
 local editor_directory  = editor_defaults.directory
+local verbose_mode      = args.verbose
+
+if verbose_mode then
+  logger:setLevel (logging.DEBUG)
+else
+  logger:setLevel (logging.INFO)
+end
+
+logger:info ("Dispatcher URL is '" .. dispatcher_url .. "'.")
+logger:info ("Root directory is '" .. root_directory .. "'.")
+logger:info ("Resource is '" .. resource .. "'.")
+logger:info ("Dispatcher token is '" .. dispatcher_token .. "'.")
+logger:info ("Editor token is '" .. editor_token .. "'.")
+logger:info ("Editor port is '" .. tostring (editor_port) .. "'.")
+logger:info ("Editor directory is '" .. editor_directory .. "'.")
 
 function json.read (filename)
   local file = io.open (filename, "r")
@@ -85,7 +108,12 @@ end
 
 local string_mt = getmetatable ""
 function string_mt:__call (parameters)
-  return (self:gsub('($%b{})', function(w) return parameters[w:sub(3, -2)] or w end))
+  return self:gsub (
+    '($%b{})',
+    function (w)
+      return parameters[w:sub(3, -2)] or w
+    end
+  )
 end
 
 
@@ -96,49 +124,81 @@ local info_filename = ("${root}/${resource}/info.json") {
 }
 local info = json.read (info_filename)
 if not info then
-  error ("File " .. info_filename .. " does not exist.")
+  logger:warning ("File " .. info_filename .. " does not exist.")
+  info = {}
 end
 if info.url then
   local client = websocket.client.sync { timeout = 2 }
   local ok, err = client:connect(info.url, 'cosy')
   if ok then
-    print ("Editor is already running at " .. info.url .. ".")
+    logger:info ("Editor is already running at " .. info.url .. ".")
     os.exit (0) -- editor already exists
   else
+    logger:debug ("Editor is not running at " .. info.url ..
+                  ", because " .. err .. ".")
     info.url = nil
   end
+  client:close ()
 end
 
 -- Generate Dockerfile
-local docker = ([[
-FROM saucisson/cosy-editor:testing-amd64
-MAINTAINER alban.linard@lsv.ens-cachan.fr
+do
+  local docker = ([[
+  FROM saucisson/cosy-editor:testing-amd64
+  MAINTAINER alban.linard@lsv.ens-cachan.fr
 
-USER cosyverif
-RUN mkdir -p ${directory}
-ADD model.lua     ${directory}/model.lua
-ADD model.version ${directory}/model.version
-ADD patches       ${directory}/patches
-]]) {
-  directory = editor_directory,
-}
-local dockerfile = ("${root}/${resource}/Dockerfile") {
-  root     = root_directory,
-  resource = resource,
-}
-docker:write (dockerfile)
+  USER cosyverif
+  RUN mkdir -p ${directory}
+  ADD model.lua     ${directory}/model.lua
+  ]]) {
+    directory = editor_directory,
+  }
+  local model_file = ("${root}/${resource}/model.lua") {
+    root     = root_directory,
+    resource = resource,
+  }
+  if not lfs.attributes (model_file) then
+    string.write ("", model_file)
+  end
+  local version_file = ("${root}/${resource}/model.version") {
+    root     = root_directory,
+    resource = resource,
+  }
+  if lfs.attributes (version_file) then
+  docker = docker .. ([[
+  ADD model.version ${directory}/model.version
+  ]]) {
+    directory = editor_directory,
+  }
+  end
+  local patches_dir = ("${root}/${resource}/patches") {
+    root     = root_directory,
+    resource = resource,
+  }
+  if lfs.attributes (patches_dir) then
+  docker = docker .. ([[
+  ADD patches       ${directory}/patches
+  ]]) {
+    directory = editor_directory,
+  }
+  end
+  local dockerfile = ("${root}/${resource}/Dockerfile") {
+    root     = root_directory,
+    resource = resource,
+  }
+  docker:write (dockerfile)
+end
 
 local tag = sha1 (resource)
-
 do
   local command = ([[
-    docker.io build --force-rm --rm --quiet --tag=${tag} ${root}/${resource}
+    docker.io build --force-rm --rm --quiet --tag=${tag} ${root}/${resource} > /dev/null
   ]]) {
     root     = root_directory,
     resource = resource,
     tag      = tag
   }
-  print (command)
+  logger:debug (command)
   os.execute (command)
 end
 
@@ -156,13 +216,14 @@ end
 
 do
   local command = ([[
-    editor="cosy-editor ${token}"
+    editor="cosy-editor -t 10 ${token}"
     docker.io run --detach --publish ${port}  ${image} ${editor}
   ]]) {
     port     = editor_port,
     token    = editor_token,
     image    = tag,
   }
+  logger:debug (command)
   cid = execute (command) [1]
 end
 
@@ -170,9 +231,10 @@ do
   local command = ([[
     docker.io port ${cid} ${port}
   ]]) {
-    port = editor_port,
+    port = editor_port, 
     cid  = cid,
   }
+  logger:debug (command)
   url = "ws://" .. (execute (command) [1]) .. "/"
 end
 
@@ -181,7 +243,7 @@ json.write (info, info_filename)
 
 do
   local client = websocket.client.sync { timeout = 2 }
-  local ok, err = client:connect(dispatcher_url, 'cosy')
+  local ok, err = client:connect (dispatcher_url, "cosy")
   if ok then
     client:send (json.encode {
       token     = dispatcher_token,
@@ -190,24 +252,43 @@ do
       url       = info.url,
     })
   else
+    logger:debug (err)
     os.exit (2)
   end
+  client:close ()
 end
 
-local command = ([[
-  nohup bash -c "
-  rm -f ${root}/${resource}/Dockerfile
-  docker.io wait '${cid}'
-  docker.io cp ${cid}:${directory}/model.lua     ${root}/${resource}/
-  docker.io cp ${cid}:${directory}/model.version ${root}/${resource}/
-  docker.io cp ${cid}:${directory}/patches       ${root}/${resource}/
-  docker.io rm ${cid}
-  docker.io rmi $(docker.io images | grep '${resource}' | tr -s ' ' | cut -f 3 -d ' ')
-  " &
-]]) {
-  cid       = cid,
-  root      = root,
-  directory = directory,
-  resource  = resource,
-}
-execute (command)
+local script_file
+do
+  local command = [[
+    mktemp
+  ]]
+  logger:debug (command)
+  script_file = execute (command) [1]
+  local script = ([[
+    #! /bin/bash
+    rm -f ${root}/${resource}/Dockerfile
+    docker.io wait '${cid}'
+    docker.io cp ${cid}:${directory}/model.lua     ${root}/${resource}/
+    docker.io cp ${cid}:${directory}/model.version ${root}/${resource}/
+    docker.io cp ${cid}:${directory}/patches       ${root}/${resource}/
+    docker.io rm ${cid}
+    docker.io rmi $(docker.io images | grep '${resource}' | tr -s ' ' | cut -f 3 -d ' ')
+    rm -f ${script_file}
+  ]]) {
+    cid         = cid,
+    root        = root_directory,
+    directory   = editor_directory,
+    resource    = resource,
+    script_file = script_file,
+  }
+  string.write (script, script_file)
+  command = ([[
+    chmod a+x ${script_file}
+    bash -c "nohup ${script_file} > /dev/null 2>&1 &"
+  ]]) {
+    script_file = script_file,
+  }
+  logger:debug (command)
+  execute (command)
+end
